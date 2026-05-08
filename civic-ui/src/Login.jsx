c4ebbd4fa5cc281a09e371f44a415d369ff02896
sasmitha0508 <sasmithar08@gmail.com>
2026-05-08 02:41:31 -0400

    Fixed CSS import case issue

--- civic-ui/src/Login.jsx
+++ civic-ui/src/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SignUp.css'; // Reusing the same CSS file
+import './signup.css'; // Reusing the same CSS file
 import logo from './assets/logo2.png';
 
 // Language translations
