4c5269579f2d904a82e08fce5755523d0cc80c3b
sasmitha0508 <sasmithar08@gmail.com>
2026-05-22 21:26:28 -0400

    fix railway backend port

--- server/server.js
+++ server/server.js
@@ -8,10 +8,6 @@ app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
 
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK" });
-});
-
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
